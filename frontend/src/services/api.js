c49e28ff7d39d8a3e04a626f2519486a62f67607
karthik1Srigadha <srigadha.karthik@gmail.com>
2025-11-22 17:25:35 +0530

    Initial HRMS project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hrms-fullstack-1.onrender.com",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Add token to all requests
