93cd7c21dd539caeccc3d42da820a93522f9c9de
karthik1Srigadha <srigadha.karthik@gmail.com>
2025-11-22 17:34:16 +0530

    Initial HRMS project

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,12 @@ const logRoutes = require("./routes/logs");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hrms-fullstack-frontend.vercel.app",  // <-- your frontend vercel url
+    ],
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   })
 );
